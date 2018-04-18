d380f69d7c63a36b23fcdac849e77b5f8d17eb18
Bradford D. Boyle <bradford.d.boyle@gmail.com>
2018-04-17 23:37:49 -0700

    Basic command for refreshing tokens
    TODO: automatically renew token

--- src/main.rs
+++ src/main.rs
@@ -85,6 +85,72 @@ impl FitbitClient {
     }
 }
 
+struct FitbitAuth(oauth2::Config);
+
+impl FitbitAuth {
+    fn new(client_id: &str, client_secret: &str) -> FitbitAuth {
+        let auth_url = "https://www.fitbit.com/oauth2/authorize";
+        let token_url = "https://api.fitbit.com/oauth2/token";
+        // let token_url = "http://localhost:8080";
+
+        // Set up the config for the Github OAuth2 process.
+        let mut config = Config::new(client_id, client_secret, auth_url, token_url);
+
+        // config = config.set_response_type(ResponseType::Token);
+        config = config.set_auth_type(AuthType::BasicAuth);
+
+        // This example is requesting access to the user's public repos and email.
+        config = config.add_scope("activity");
+        config = config.add_scope("heartrate");
+        config = config.add_scope("profile");
+
+        // This example will be running its own server at localhost:8080.
+        // See below for the server implementation.
+        config = config.set_redirect_url("http://localhost:8080");
+
+        FitbitAuth(config)
+    }
+
+    fn get_token(&self) -> Result<oauth2::Token, String> {
+        let authorize_url = self.0.authorize_url();
+
+        println!(
+            "Open this URL in your browser:\n{}\n",
+            authorize_url.to_string()
+        );
+
+        // FIXME avoid unwrap here
+        let server = tiny_http::Server::http("localhost:8080").unwrap();
+        let request = server.recv().map_err(stringify)?;
+        let url = request.url().to_string();
+        let response = tiny_http::Response::from_string("Go back to your terminal :)");
+        request.respond(response).map_err(stringify)?;
+
+        let code = {
+            // remove leading '/?'
+            let mut parsed = url::form_urlencoded::parse(url[2..].as_bytes());
+
+            let (_, value) = parsed
+                .find(|pair| {
+                    let &(ref key, _) = pair;
+                    key == "code"
+                })
+                .ok_or("query param `code` not found")?;
+            value.to_string()
+        };
+
+        // Exchange the code with a token.
+        self.0.exchange_code(code).map_err(stringify)
+    }
+
+    fn exchange_refresh_token(&self, token: Token) -> Result<oauth2::Token, String> {
+        match token.0.refresh_token {
+            Some(t) => self.0.exchange_refresh_token(t).map_err(stringify),
+            None => Err("No refresh token available".to_string()),
+        }
+    }
+}
+
 fn main() {
     let matches = App::new("Fitbit Grabber")
         .subcommand(
@@ -108,7 +174,27 @@ fn main() {
             ),
         )
         .subcommand(SubCommand::with_name("token").about("request an access token"))
+        .subcommand(SubCommand::with_name("refresh-token").about("refresh token"))
         .get_matches();
+
+    let fitbit_client_id =
+        env::var("FITBIT_CLIENT_ID").expect("Missing the FITBIT_CLIENT_ID environment variable.");
+    let fitbit_client_secret = env::var("FITBIT_CLIENT_SECRET")
+        .expect("Missing the FITBIT_CLIENT_SECRET environment variable.");
+    let auth = FitbitAuth::new(&fitbit_client_id, &fitbit_client_secret);
+
+    if let Some(_) = matches.subcommand_matches("token") {
+        let token = auth.get_token().expect("unable to get a new token");
+        save_token(".token", token)
+    }
+
+    if let Some(_) = matches.subcommand_matches("refresh-token") {
+        let old_token = load_token(".token").unwrap();
+        let token = auth.exchange_refresh_token(old_token)
+            .expect("unable to exchange refresh token for new access token");
+
+        save_token(".token", token);
+    }
 
     let token = load_token(".token").unwrap();
     let f = FitbitClient::new(token);
@@ -130,28 +216,21 @@ fn main() {
             .unwrap();
         println!("{}", f.step(date).unwrap());
     }
+}
 
-    if let Some(_) = matches.subcommand_matches("token") {
-        let fitbit_client_id = env::var("FITBIT_CLIENT_ID")
-            .expect("Missing the FITBIT_CLIENT_ID environment variable.");
-        let fitbit_client_secret = env::var("FITBIT_CLIENT_SECRET")
-            .expect("Missing the FITBIT_CLIENT_SECRET environment variable.");
+fn save_token(filename: &str, token: oauth2::Token) {
+    let json = serde_json::to_string(&token).unwrap();
+    // TODO save the token as JSON
+    let path = Path::new(".token");
+    let display = path.display();
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
 
-        let token = get_token(&fitbit_client_id, &fitbit_client_secret).unwrap();
-        let json = serde_json::to_string(&token).unwrap();
-        // TODO save the token as JSON
-        let path = Path::new(".token");
-        let display = path.display();
-        let mut file = match File::create(&path) {
-            Err(why) => panic!("couldn't create {}: {}", display, why.description()),
-            Ok(file) => file,
-        };
-
-        match file.write_all(json.as_bytes()) {
-            Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
-            Ok(_) => (),
-        }
-        println!("{}", json);
+    match file.write_all(json.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
+        Ok(_) => (),
     }
 }
 
