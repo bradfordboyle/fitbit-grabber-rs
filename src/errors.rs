72da9d656a5a933693de96a4b77c55d2f4b9c797
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-06-01 16:13:16 -0700

    Restructure crate to contain lib and bin
    The binary name will be the same. The binary can be built like
    
    ```
    cargo build --bin fitbit-grabber
    ```
    
    The new library name is "fitbit", and src/bin/main.rs imports it. We could
    configure to have more binaries, but only one top-level library.
    
    Error handling:
    Convert errors to failure crate.
    
    On the library side, wrap errors from our dependencies with an enum, and derive
    Fail for it. This approach to lets us introduce strong types with custom
    printing fairly quickly. It has the disadvantage of being somewhat rigid, and
    is one of a few approaches described here:
    https://boats.gitlab.io/failure/custom-fail.html
    
    On the binary side, as a user of the library, failure allows us to make liberal
    use of the `?` operator, even in main, since main can do `Result` now.
    
    Config changes, allow a config file:
    Create application config module
    
    config::Config can be deserialized from a config file at
    
    ~/.config/fitbit-grabber/conf.toml
    
    or set with --config at the command line.
    
    The config struct is all Options that must be unwrapped, so we could have
    separate command funcs that care about particular values on a Config.

---
+++ src/errors.rs
@@ -0,0 +1,45 @@
+use reqwest;
+use std::convert::From;
+use url;
+
+#[derive(Fail, Debug)]
+pub enum Error {
+    #[fail(display = "http err: {}", _0)]
+    Http(#[cause] ::reqwest::Error),
+
+    #[fail(display = "io error: {}", _0)]
+    Io(#[cause] ::std::io::Error),
+
+    #[fail(display = "url err: {}", _0)]
+    Url(#[cause] ::url::ParseError),
+
+    #[fail(display = "oauth token err: {}", _0)]
+    AuthToken(#[cause] ::oauth2::TokenError),
+
+    #[fail(display = "refresh token missing")]
+    RefreshTokenMissing,
+
+    #[fail(display = "missing code param ")]
+    OAuthCodeMissing,
+
+    #[fail(display = "bad date format: {}", _0)]
+    DateParse(#[cause] ::chrono::ParseError),
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(kind: reqwest::Error) -> Error {
+        Error::Http(kind)
+    }
+}
+
+impl From<::std::io::Error> for Error {
+    fn from(kind: ::std::io::Error) -> Error {
+        Error::Io(kind)
+    }
+}
+
+impl From<url::ParseError> for Error {
+    fn from(kind: url::ParseError) -> Error {
+        Error::Url(kind)
+    }
+}
